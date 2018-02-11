5be310d907b97d4830a7617e0e3e674e3f48b50c
Zenzamy Othmane <oz.elaamri@gmail.com>
2018-02-11 00:40:47 +0000

    First push

---
+++ src/Controller/HelloController.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace Drupal\hello_world\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+class HelloController extends ControllerBase {
+
+   public function hello() {
+     
+     return array(
+	"#type" => "markup",
+	"#markup" => $this->t('Hello World')
+     
+     );
+   
+   }
+
+
+}
